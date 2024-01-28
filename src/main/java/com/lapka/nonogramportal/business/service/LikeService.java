8ef99328e206fe60343d981d183d3207dd2f3152
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2024-01-28 23:04:13 +0200

    Server: Removed unused imports

--- src/main/java/com/lapka/nonogramportal/business/service/LikeService.java
+++ src/main/java/com/lapka/nonogramportal/business/service/LikeService.java
@@ -2,9 +2,6 @@ package com.lapka.nonogramportal.business.service;
 
 import com.lapka.nonogramportal.model.Like;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 public interface LikeService {
     ResponseEntity<Integer> getLikesCountByNonogramId(Long nonogramId);
